b77d8af6e12b34cc1ebbc62b17121620f61fc5d6
Keeper9 <jermaine.n.reid@gmail.com>
2018-08-01 07:41:00 -0400

    First commit

---
+++ Labs/MadLibs/script.js
@@ -0,0 +1,23 @@
+function init()
+{
+	/*Add Event Listener to button element that calls generateMsg() function*/
+}
+
+function generateMsg()
+{
+	//Create a constant array of the ids of the input textfields.*/
+	
+ 	let values = [];
+	
+
+	/*Using a for loop, populate the values array with the values of the
+	 textfields*/
+
+	
+	let msg = `If anybody is wondering, please know that I am <strong>${values[2]}</strong> at <strong>${values[4]}</strong> without you.
+	You might want to consider <strong>${values[3]}</strong> <strong>${values[0]}</strong>... then again, you always preferred 
+	<strong>${values[1]}</strong>.`;
+	
+	//Display the msg string in the paragraph element with id 'msg'
+}
+
